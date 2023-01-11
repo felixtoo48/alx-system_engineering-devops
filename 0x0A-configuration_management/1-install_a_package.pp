c387871cc13cf3eb26885493c54441634510d6f4
felixtoo48 <felixtoo48@gmail.com>
2023-01-11 23:07:24 +0300

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using puppet, install flask of version 2.1.0
 
 exec { 'install python packages':
-  ensure  => '2.1.0',
   command => 'pip3 install flask==2.1.0',
   path    => ['/usr/bin/'],
   unless  => '/usr/bin/test -f /usr/local/lib/python3.8/dist-packages/flask/app.py',
