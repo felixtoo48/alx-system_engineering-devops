8172abaf398f8c8aa77e7aabd79c2d49c6f5180f
felixtoo48 <felixtoo48@gmail.com>
2023-08-08 16:40:05 +0300

    correcting documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """query reddit api"""
 import requests
+import sys
 
 proxies = {
     'http': 'http://your_proxy_here',
     'https': 'http://your_proxy_here'
 }
+
 
 def number_of_subscribers(subreddit):
     # Set a custom User-Agent to avoid Too Many Requests errors
@@ -15,20 +17,14 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
+            return (data.get('data').get('subscribers'))
         else:
             return 0
 
     except Exception as e:
         print(f"An error occurred: {e}")
         return 0
-
-# Test the function
-subreddit_name = 'programming'
-subscribers_count = number_of_subscribers(subreddit_name)
-print(f"Subscribers in '{subreddit_name}': {subscribers_count}")
