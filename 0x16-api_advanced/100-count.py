1f6214295c430f6750f35ab3ab2cb5180491024b
felixtoo48 <felixtoo48@gmail.com>
2023-08-08 17:56:01 +0300

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
-"""recursive function that queries reddit api"""
-
+""" recursive function that queries the Reddit API"""
 import requests
 import sys
+after = None
+count_dic = []
 
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    # Set a custom User-Agent to avoid Too Many Requests errors
-    headers = {'User-Agent': 'Your User-Agent Here'}
-
-    if count_dict is None:
-        count_dict = {}
-
-    # Construct the API URL for the subreddit's hot posts
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
-    if after:
-        url += f'&after={after}'
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-
-            if len(posts) == 0:
-                return count_dict
-
-            for post in posts:
-                title = post['data']['title']
-                for word in word_list:
-                    word = word.lower()
-                    if f" {word.lower()} " in f" {title.lower()} ":
-                        count_dict[word] = count_dict.get(word, 0) + 1
-
-            # Check if there are more pages to fetch
-            after = data['data']['after']
-            if after:
-                return count_words(subreddit, word_list, after, count_dict)
-            else:
-                return count_dict
-
-        else:
-            return None
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
