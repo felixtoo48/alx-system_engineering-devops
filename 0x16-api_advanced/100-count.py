6605204c078b596fadc641f449760aed4b9fa952
felixtoo48 <felixtoo48@gmail.com>
2023-08-08 19:33:46 +0300

    error

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,54 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""Function to count words """""
+
 import requests
 import sys
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+def count_words(subreddit, word_list, after=None, word_count=None):
+    # Set a custom User-Agent to avoid Too Many Requests errors
+    headers = {'User-Agent': 'Your User-Agent Here'}
+
+    if word_count is None:
+        word_count = {}
+
+    # Construct the API URL for the subreddit's hot posts
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
+    if after:
+        url += f'&after={after}'
+
     try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+        response = requests.get(url, headers=headers)
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
+            if len(posts) == 0:
+                sorted_results = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+                for word, count in sorted_results:
+                    print(f"{word}: {count}")
+                return
+
+            for post in posts:
+                title = post['data']['title']
+                words = title.lower().split()
+
+                for word in word_list:
+                    if word in words:
+                        if word in word_count:
+                            word_count[word] += 1
+                        else:
+                            word_count[word] = 1
+
+            # Check if there are more pages to fetch
+            after = data['data']['after']
+            return count_words(subreddit, word_list, after, word_count)
+
+        else:
+            print("Invalid subreddit.")
             return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
