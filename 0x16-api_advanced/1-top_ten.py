a0dd5c57560596de71393bf551d6fe77fb34edce
felixtoo48 <felixtoo48@gmail.com>
2023-08-08 16:57:58 +0300

    removing parameters

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,8 +13,7 @@ def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False,
-                                params=parameters)
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
