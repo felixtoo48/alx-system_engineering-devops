c6a9d5915439d095caa776e2b963e554db21e1d6
felixtoo48 <felixtoo48@gmail.com>
2023-08-08 20:48:43 +0300

    error

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Function to count words """""
+"""100-count"""
 
 import requests
 import sys
+import json
+
+
+def count_languages_recursive(data, language_counts):
+    if isinstance(data, dict):
+        if "title" in data.get("data", {}):
+            title = data["data"]["title"].lower()
+            for language in language_counts:
+                if language in title:
+                    language_counts[language] += 1
+        for key, value in data.items():
+            count_languages_recursive(value, language_counts)
+    elif isinstance(data, list):
+        for item in data:
+            count_languages_recursive(item, language_counts)
 
 
 def count_words(subreddit, word_list, after=None, word_count=None):
-    # Set a custom User-Agent to avoid Too Many Requests errors
-    headers = {'User-Agent': 'Your User-Agent Here'}
-
     if word_count is None:
         word_count = {}
 
-    # Construct the API URL for the subreddit's hot posts
+    headers = {'User-Agent': 'Your User-Agent Here'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
     if after:
         url += f'&after={after}'
@@ -41,13 +53,31 @@ def count_words(subreddit, word_list, after=None, word_count=None):
                         else:
                             word_count[word] = 1
 
-            # Check if there are more pages to fetch
             after = data['data']['after']
             return count_words(subreddit, word_list, after, word_count)
 
+        # Sample JSON data containing Reddit post information
+        reddit_posts = [print(response.content)]
+
+        # Dictionary to store programming language counts
+        programming_languages = {
+                "java": 0,
+                "javascript": 0,
+                "python": 0,
+                "react": 0,
+                "scala": 0
+            }
+
+        # Count programming languages recursively
+        count_languages_recursive(reddit_posts, programming_languages)
+
+        # Print the programming language counts
+        for language, count in programming_languages.items():
+            print(f"{language}: {count}")
+        
         else:
-            print("Invalid subreddit.")
             return
+
 
     except Exception as e:
         print(f"An error occurred: {e}")
